5b9266f703b1488da9a034bce3d0f2bde6b1d669
Vasek Sraier <git@vakabus.cz>
2020-09-26 18:20:47 +0200

    tooling: fixed rollup and package.json

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -5,6 +5,7 @@ import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -38,6 +39,8 @@ export default {
 		file: 'public/build/bundle.js'
 	},
 	plugins: [
+		nodePolyfills(),
+		
 		svelte({
 			// enable run-time checks when not in production
 			dev: !production,
