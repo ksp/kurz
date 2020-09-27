10ef0fc4e4f464f9ec6d3510a1d34f02266f210e
exyi <exyi@outlook.cz>
2020-09-27 11:08:37 +0000

    Handle tasks.json by a controller

---
+++ server/Ksp.WebServer/Controllers/TasksController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Ksp.WebServer.Controllers
+{
+    [ApiController]
+    [Route("tasks.json")]
+    public class TasksController : ControllerBase
+    {
+        private readonly ILogger<TasksController> logger;
+        private readonly IWebHostEnvironment env;
+
+        public TasksController(ILogger<TasksController> logger, IWebHostEnvironment env)
+        {
+            this.env = env;
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return this.PhysicalFile(Path.Combine(env.ContentRootPath, "../../tasks.json"), "text/json");
+        }
+    }
+}
