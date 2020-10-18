9f72e5393bfe92a6abe426ce4e2623a412c7e3a8
exyi <exyi@outlook.cz>
2020-10-18 09:47:23 +0000

    Ok, browsers are ignoring pattern, lets use type=number

--- frontend/src/SolutionCaptcha.svelte
+++ frontend/src/SolutionCaptcha.svelte
@@ -12,7 +12,21 @@
     }
 </script>
 
+<style>
+     /* Chrome, Safari, Edge, Opera */
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+    /* Firefox */
+    input[type=number] {
+        -moz-appearance: textfield;
+    }
+</style>
+
 <div>
     <p>Úlohu zatím nemáš vyřešenou, opravdu si chceš vyzradit řešení?</p>
-    {a} + {b} = <input type=text pattern="[0-9]*" on:input={e => newValue(+e.currentTarget.value)}>
+    {a} + {b} = <input type="number" on:input={e => newValue(+e.currentTarget.value)}>
 </div>
