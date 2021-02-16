3b318600aac366b828f13585d9dd08dbde08fdfb
Vasek Sraier <git@vakabus.cz>
2021-02-16 23:16:12 +0100

    ksp web binding: předělán binding do KSPího layoutu, aby se nerozbíjela CSSka

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,14 @@
 import App from './App.svelte';
 
-document.getElementById("page").innerHTML = "";
+// cleanup basic KSP layout while preserving exactly the header
+let header = document.getElementById("header")!;
+document.body.innerHTML = "";
+document.body.appendChild(header);
+let appRoot = document.createElement("div");
+document.body.appendChild(appRoot);
 
 const app = new App({
-	target: document.getElementById("page")!,
+	target: appRoot,
 	props: { }
 });
 
